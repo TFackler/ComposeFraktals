03d2ff954fa5356355090b8ace6ba0dfd6738ef5
TFackler <38538728+TFackler@users.noreply.github.com>
2022-03-14 18:46:22 +0100

    :recycle: Optimize drawing method for SierpinskiTriangle

--- src/main/kotlin/fractals/SierpinskiTriangle.kt
+++ src/main/kotlin/fractals/SierpinskiTriangle.kt
@@ -76,41 +76,63 @@ private fun ColumnScope.SierpinskiTriangleCanvas(iterations: Int? = null) {
     }
 }
 
-private fun sameSidedTriangleWidth(height: Float): Float {
-    // sqrt(4.0f / 3.0f * height.pow(2))
-    return 1.1547005f * height
-}
-
-private fun DrawScope.drawTri(bottomLeft: Offset, size: Float, color: Color) {
-    val path = Path()
-    path.moveTo(bottomLeft.x, bottomLeft.y)
-    path.relativeLineTo(size, 0f)
-    val height = sameSidedTriangleHeight(size)
-    path.relativeLineTo(-size / 2, -height)
-    path.close()
-    drawPath(path = path, color = color, style = Stroke(width = 1.0f))
-}
-
 private fun DrawScope.drawSierpinskiTri(
     bottomLeft: Offset,
     size: Float,
     iterationDepth: Int? = null,
     foregroundColor: Color
 ) {
-    if ((iterationDepth != null && iterationDepth <= 0) || size < 2.0f) {
+    if (iterationDepth != null && iterationDepth <= 0) {
         return
     }
-    drawTri(bottomLeft, size, foregroundColor)
+    val triangle = createSameSidedTri(bottomLeft, size)
+    drawTri(triangle.first, triangle.second, triangle.third, foregroundColor)
+    drawSierpinskiRecursive(triangle.first, triangle.second, triangle.third, iterationDepth?.minus(1), foregroundColor)
+}
 
-    val newSize = size / 2.0f
-    drawSierpinskiTri(bottomLeft, newSize, iterationDepth?.minus(1), foregroundColor)
+private fun createSameSidedTri(bottomLeft: Offset, sideLength: Float): Triple<Offset, Offset, Offset> {
+    val bottomRight = bottomLeft + Offset(sideLength, 0f)
+    val top = bottomLeft + Offset(sideLength / 2f, -sameSidedTriangleHeight(sideLength))
+    return Triple(bottomLeft, bottomRight, top)
+}
 
-    val bottomCenter = Offset(bottomLeft.x + newSize, bottomLeft.y)
-    drawSierpinskiTri(bottomCenter, newSize, iterationDepth?.minus(1), foregroundColor)
+private fun DrawScope.drawSierpinskiRecursive(
+    bottomLeft: Offset,
+    bottomRight: Offset,
+    top: Offset,
+    iterationDepth: Int?,
+    color: Color
+) {
+    if (iterationDepth != null && iterationDepth <= 0) {
+        return
+    }
+    if ((bottomLeft - bottomRight).getDistanceSquared() < 4.0f) {
+        return
+    }
+    // inner upside down triangle
+    val bottom = bottomLeft + ((bottomRight - bottomLeft) / 2f)
+    val topLeft = bottomLeft + ((top - bottomLeft) / 2f)
+    val topRight = bottomRight + ((top - bottomRight) / 2f)
+    drawTri(bottom, topLeft, topRight, color)
 
-    val height = sameSidedTriangleHeight(size) / 2.0f
-    val upperTriBottomLeft = Offset(bottomLeft.x + newSize / 2.0f, bottomLeft.y - height)
-    drawSierpinskiTri(upperTriBottomLeft, newSize, iterationDepth?.minus(1), foregroundColor)
+    // recursive calls for the next 3 triangles
+    drawSierpinskiRecursive(bottomLeft, topLeft, bottom, iterationDepth?.minus(1), color)
+    drawSierpinskiRecursive(bottom, topRight, bottomRight, iterationDepth?.minus(1), color)
+    drawSierpinskiRecursive(topRight, topLeft, top, iterationDepth?.minus(1), color)
+}
+
+private fun DrawScope.drawTri(a: Offset, b: Offset, c: Offset, color: Color) {
+    val path = Path()
+    path.moveTo(a.x, a.y)
+    path.lineTo(b.x, b.y)
+    path.lineTo(c.x, c.y)
+    path.close()
+    drawPath(path, color, style = Stroke(1.0f))
+}
+
+private fun sameSidedTriangleWidth(height: Float): Float {
+    // sqrt(4.0f / 3.0f * height.pow(2))
+    return 1.1547005f * height
 }
 
 private fun sameSidedTriangleHeight(size: Float) = sqrt(3f * size.pow(2) / 4f)
