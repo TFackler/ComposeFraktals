61c3c1f809bb7e5de2f7b4b323a2a452978383f9
TFackler <38538728+TFackler@users.noreply.github.com>
2022-03-14 23:41:28 +0100

    :sparkles: Add sierpinski carpet

--- src/main/kotlin/fractals/SierpinskiTriangle.kt
+++ src/main/kotlin/fractals/SierpinskiTriangle.kt
@@ -18,13 +18,19 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import components.LabeledSlider
 import components.RadioButtonGroup
+import fractals.SierpinskiTriangle.MIN_DRAW_SIZE
+import java.lang.Float.min
 import kotlin.math.pow
 import kotlin.math.sqrt
+
+object SierpinskiTriangle {
+    const val MIN_DRAW_SIZE = 2.0f
+}
 
 @Composable
 fun SierpinskiTriangle() {
     var iterations by remember { mutableStateOf(0f) }
-    var mode by remember { mutableStateOf(SierpinskiMode.TRIANGLE) }
+    var mode by remember { mutableStateOf(SierpinskiMode.CARPET) }
 
     Column(
         modifier = Modifier.padding(20.dp).border(2.dp, Color.Green, RectangleShape)
@@ -69,11 +75,70 @@ private fun ColumnScope.SierpinskiCarpetCanvas(iterations: Int? = null) {
             .weight(1.0f)
     ) {
         val canvasCenter = Offset(size.width / 2f, size.height / 2f)
-        drawRect(
-            color = canvasForeground,
-            topLeft = canvasCenter.minus(Offset(100f, 100f)),
-            size = Size(200f, 200f)
+        val sideLength = min(size.width, size.height)
+
+        drawSierpinskiCarpet(
+            Offset(canvasCenter.x - sideLength / 2f, canvasCenter.y - sideLength / 2f),
+            sideLength,
+            iterations,
+            foregroundColor = canvasForeground
         )
+    }
+}
+
+private fun DrawScope.drawSierpinskiCarpet(
+    topLeft: Offset,
+    size: Float,
+    iterationDepth: Int? = null,
+    foregroundColor: Color
+) {
+    if (iterationDepth != null && iterationDepth <= 0) {
+        return
+    }
+    drawRect(
+        color = foregroundColor,
+        topLeft = topLeft,
+        size = Size(size, size),
+        style = Stroke(1.0f),
+    )
+    drawSierpinskiCarpetRecursive(
+        topLeft,
+        size,
+        iterationDepth?.minus(1),
+        foregroundColor,
+    )
+}
+
+private fun DrawScope.drawSierpinskiCarpetRecursive(
+    topLeft: Offset,
+    size: Float,
+    iterationDepth: Int? = null,
+    color: Color
+) {
+    if ((iterationDepth != null && iterationDepth <= 0) || size <= MIN_DRAW_SIZE) {
+        return
+    }
+    // inner rectangle
+    val innerSize = size / 3f
+    val innerTopLeft = topLeft + Offset(innerSize, innerSize)
+    drawRect(
+        color,
+        innerTopLeft,
+        Size(innerSize, innerSize),
+        style = Stroke(1.0f),
+    )
+    // recursive calls for the next 8 squares
+    for (i in 0..2) {
+        for (j in 0..2) {
+            if (!(i == 1 && i == j)) {
+                drawSierpinskiCarpetRecursive(
+                    topLeft + Offset(innerSize * i, innerSize * j),
+                    innerSize,
+                    iterationDepth?.minus(1),
+                    color
+                )
+            }
+        }
     }
 }
 
@@ -89,8 +154,6 @@ private fun ColumnScope.SierpinskiTriangleCanvas(iterations: Int? = null) {
             .fillMaxWidth()
             .weight(1.0f)
     ) {
-
-
         val canvasCenter = Offset(size.width / 2f, size.height / 2f)
         val triSize = if (sameSidedTriangleHeight(size.width) < size.height) {
             Size(size.width, sameSidedTriangleHeight(size.width))
@@ -118,7 +181,13 @@ private fun DrawScope.drawSierpinskiTri(
     }
     val triangle = createSameSidedTri(bottomLeft, size)
     drawTri(triangle.first, triangle.second, triangle.third, foregroundColor)
-    drawSierpinskiRecursive(triangle.first, triangle.second, triangle.third, iterationDepth?.minus(1), foregroundColor)
+    drawSierpinskiTriRecursive(
+        triangle.first,
+        triangle.second,
+        triangle.third,
+        iterationDepth?.minus(1),
+        foregroundColor
+    )
 }
 
 private fun createSameSidedTri(bottomLeft: Offset, sideLength: Float): Triple<Offset, Offset, Offset> {
@@ -127,17 +196,14 @@ private fun createSameSidedTri(bottomLeft: Offset, sideLength: Float): Triple<Of
     return Triple(bottomLeft, bottomRight, top)
 }
 
-private fun DrawScope.drawSierpinskiRecursive(
+private fun DrawScope.drawSierpinskiTriRecursive(
     bottomLeft: Offset,
     bottomRight: Offset,
     top: Offset,
     iterationDepth: Int?,
     color: Color
 ) {
-    if (iterationDepth != null && iterationDepth <= 0) {
-        return
-    }
-    if ((bottomLeft - bottomRight).getDistanceSquared() < 4.0f) {
+    if ((iterationDepth != null && iterationDepth <= 0) || (bottomLeft - bottomRight).getDistanceSquared() < MIN_DRAW_SIZE * MIN_DRAW_SIZE) {
         return
     }
     // inner upside down triangle
@@ -147,9 +213,9 @@ private fun DrawScope.drawSierpinskiRecursive(
     drawTri(bottom, topLeft, topRight, color)
 
     // recursive calls for the next 3 triangles
-    drawSierpinskiRecursive(bottomLeft, topLeft, bottom, iterationDepth?.minus(1), color)
-    drawSierpinskiRecursive(bottom, topRight, bottomRight, iterationDepth?.minus(1), color)
-    drawSierpinskiRecursive(topRight, topLeft, top, iterationDepth?.minus(1), color)
+    drawSierpinskiTriRecursive(bottomLeft, topLeft, bottom, iterationDepth?.minus(1), color)
+    drawSierpinskiTriRecursive(bottom, topRight, bottomRight, iterationDepth?.minus(1), color)
+    drawSierpinskiTriRecursive(topRight, topLeft, top, iterationDepth?.minus(1), color)
 }
 
 private fun DrawScope.drawTri(a: Offset, b: Offset, c: Offset, color: Color) {
